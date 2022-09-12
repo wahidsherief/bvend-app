3fbd4fcbce92479b1a1173d4b7b3bd5b7093c42c
WahidSherief <cengr.wahid@gmail.com>
2022-09-13 00:31:16 +0600

    create hoc

--- src/app/components/common/ListContainer.js
+++ src/app/components/common/ListContainer.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react"
-import PageTitle from "../common/PageTitle"
-import { useSelector, useDispatch } from "react-redux"
-import { remove } from "../../../features/product/product_list_slice"
-import EditForm from "./EditForm";
+import { useDispatch } from "react-redux"
+// import { remove } from "../../../features/item/item_list_slice"
 
 
-const ProductList = () => {
-    const productListState = useSelector((state) => {
-        return state['product_list']
-    })
 
-    const dispatch = useDispatch()
-    const { products } = productListState
+const ListContainer = (props) => {
+
+
+
+    const { itemName, items } = props.item
+
+    console.log('xy', props)
+
+    // const dispatch = useDispatch()
 
     const [editPanel, setEditPanel] = useState(null)
     const [removePanel, setRemovePanel] = useState(null)
@@ -41,63 +42,59 @@ const ProductList = () => {
     }
 
 
-    const removeProduct = (id) => {
-        if (dispatch(remove(id)))
-            setRemovePanel(null)
-    }
+    // const removeitem = (id) => {
+    //     if (dispatch(remove(id)))
+    //         setRemovePanel(null)
+    // }
 
-    const action = {
-        hasAction: true,
-        actionTitle: 'Add Product',
-        actionLink: '/product/create'
-    }
 
     return (
         <React.Fragment>
-            <PageTitle title='Product List' action={action} />
             <div className="tables-wrapper">
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="card-style mb-30">
                             <p className="text-sm mb-20">
-                                Total products: {products.length}
+                                Total {itemName}: {items.length}
                             </p>
                             {
-                                products.length > 0 &&
-                                products.map((product) => (
-                                    <div key={product.id} className="accordion mb-2" id="accordionExample">
+                                items.length > 0 &&
+                                items.map((item) => (
+                                    <div key={item.id} className="accordion mb-2" id="accordionExample">
                                         <div className="accordion-item">
                                             <div className="accordion-header p-3" id="headingOne">
-                                                <img src={require(`../../../../public/bvend/products/${product.image}`)} className="image-in-list" alt={product.name} />
-                                                <span className="ms-3">{product.name}</span>
-                                                <span className="ms-3">{product.category}</span>
+                                                <img src={require(`../../../../public/bvend/${itemName}/${item.image}`)} className="image-in-list" alt={item.name} />
+                                                <span className="ms-3">{item.name}</span>
+                                                <span className="ms-3">{item.category}</span>
                                                 <span className="float-end">
 
-                                                    <button onClick={() => showEditPanel(product.id)} className='text-decoration-none p-0 mt-2 text-primary btn-link btn'>
+                                                    <button onClick={() => showEditPanel(item.id)} className='text-decoration-none p-0 mt-2 text-primary btn-link btn'>
                                                         Edit <i className="lni lni-pencil-alt"></i>
                                                     </button>
-                                                    <button onClick={() => showRemovePanel(product.id)} className='text-decoration-none p-0 ms-4 mt-2 text-danger btn-link btn'>
+                                                    <button onClick={() => showRemovePanel(item.id)} className='text-decoration-none p-0 ms-4 mt-2 text-danger btn-link btn'>
                                                         Delete  <i className="lni lni-trash-can"></i>
                                                     </button>
                                                 </span>
                                             </div>
                                             <div
                                                 id='edit-panel'
-                                                className={editPanel === product.id ? 'd-block' : 'd-none'}
+                                                className={editPanel === item.id ? 'd-block' : 'd-none'}
                                             >
                                                 <div className="p-4">
-                                                    <EditForm product={product} hideEditPanel={hideEditPanel} />
+                                                    {/* <EditForm item={item} /> */}
+                                                    {/* {props.editForm} */}
+                                                    {props.component}
                                                 </div>
                                             </div>
-                                            <div id='remove-panel' className={removePanel === product.id ? 'd-block' : 'd-none'}>
+                                            {/* <div id='remove-panel' className={removePanel === item.id ? 'd-block' : 'd-none'}>
                                                 <div className="accordion-body bg-light">
                                                     <h3>Are you sure to delete?</h3>
                                                     <div className="d-grid gap-2 d-md-flex justify-content-md-end mt-4">
-                                                        <button onClick={() => removeProduct(product.id)} className="main-btn danger-btn btn-hover btn-sm">Move to trash</button>
+                                                        <button onClick={() => removeitem(item.id)} className="main-btn danger-btn btn-hover btn-sm">Move to trash</button>
                                                         <button onClick={() => setRemovePanel(null)} className="main-btn dark-btn btn-hover btn-sm">Cancel</button>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </div>
                                     </div>
                                 ))
@@ -110,4 +107,4 @@ const ProductList = () => {
     )
 }
 
-export default ProductList
+export default ListContainer
