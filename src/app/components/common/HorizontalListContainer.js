57abcbd5d68182fd10d60ade793570bc767adc55
WahidSherief <cengr.wahid@gmail.com>
2022-09-19 00:05:49 +0600

    machine card designed

--- src/app/components/common/HorizontalListContainer.js
+++ src/app/components/common/HorizontalListContainer.js
@@ -1,5 +1,24 @@
 import React, { useState } from "react"
-import { ProgressBar } from "react-bootstrap"
+import Machine from "app/components/pages/vendor_panel/machine/Machine"
+
+const machines = [{
+    title: 'Machine One',
+    location: '33/A Gulshan, Dhaka',
+    image: 'machine_1.jpg',
+    totalAmount: '10,000'
+},
+{
+    title: 'Machine Two',
+    location: '33/A Banani, Dhaka',
+    image: 'machine_2.jpg',
+    totalAmount: '30,000'
+},
+{
+    title: 'Machine Three',
+    location: '33/A Rampura, Dhaka',
+    image: 'machine_3.jpg',
+    totalAmount: '20,000'
+}]
 
 const HorizontalListContainer = () => {
 
@@ -8,64 +27,7 @@ const HorizontalListContainer = () => {
             <section className="card-components">
                 <div className="container-fluid p-0">
                     <div className="row">
-                        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-                            <div className="card-style-2 mb-30">
-                                <div className="row machine-card-title-height-100">
-                                    <div className="col-lg-8 col-sm-8">
-                                        <div className="card-title">
-                                            <div className="mb-2">
-                                                <h4>Machine One</h4>
-                                            </div>
-                                            <div>
-                                                <p className="fw-light custom-small-text-12">
-                                                    Location: 33/A Gulshan, Dhaka
-                                                    <span className="ms-1 text-primary"><i className="lni lni-map-marker"></i></span>
-                                                </p>
-
-                                                <p className="fw-light mt-1 mb-1 custom-small-text-12">
-                                                    Status:
-                                                    <span className="d-inline ms-2 fsize-10 text-white p-1 rounded-pill ps-2 pe-2 bg-success">
-                                                        Active
-                                                    </span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-4 col-sm-4">
-                                        <div className="card-image">
-                                            <img
-                                                className="fit-image"
-                                                src={require(`assets/bvend/machines/machine_3.jpg`)}
-                                                alt=""
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="card-content">
-                                        <div className="row">
-                                            <div className="col-lg-5">
-                                                <h5 className="mb-2 mt-2">Refill Status</h5>
-                                            </div>
-                                            <div className="col-lg-7 d-grid custom-small-text-12 align-items-center justify-content-end">
-                                                <p className="fw-light custom-small-text-12"> Current Products: <span className='fw-700'>50/100</span></p>
-                                            </div>
-                                        </div>
-
-
-                                        <ProgressBar now={40} label={4} className='mb-1' />
-                                        <p className="fw-light custom-small-text-12"> Total Amount: <span className='fw-700'>BDT 10,000</span></p>
-                                        <div class="d-grid gap-2 mt-2">
-                                            <button class="btn btn-link" type="button">Button</button>
-                                            <div className="row">
-                                                <div className="d-grid col-lg-6"><button class="btn btn-primary" type="button">Button</button></div>
-                                                <div className="d-grid col-lg-6"><button class="btn btn-primary" type="button">Button</button></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <Machine machines={machines} />
                     </div>
                 </div>
             </section>
---
+++ src/app/components/pages/vendor_panel/machine/Machine.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { ProgressBar } from "react-bootstrap";
+
+const Machine = ({ machines }) => {
+    return (
+        <React.Fragment>
+            {machines.map((machine) => {
+                return (
+                    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                        <div className="card-style-2 mb-30">
+                            <div className="row machine-card-top-height-100">
+                                <div className="col-lg-8 col-sm-8">
+                                    <div className="card-title">
+                                        <div className="mb-2">
+                                            <h4>{machine.title}</h4>
+                                        </div>
+                                        <div>
+                                            <p className="fw-light custom-small-text-12">
+                                                Location: {machine.location}
+                                                <span className="ms-1 text-primary"><i className="lni lni-map-marker"></i></span>
+                                            </p>
+
+                                            <p className="fw-light mt-1 mb-1 custom-small-text-12">
+                                                Status:
+                                                <span className="d-inline ms-2 fsize-10 text-white p-1 rounded-pill ps-2 pe-2 bg-success">
+                                                    Active
+                                                </span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-4 col-sm-4">
+                                    <div className="card-image">
+                                        <img
+                                            className="fit-image"
+                                            src={require(`assets/bvend/machines/${machine.image}`)}
+                                            alt={machine.title}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="card-content">
+                                    <div className="row">
+                                        <div className="col-lg-5">
+                                            <h5 className="mb-2 mt-2">Refill Status</h5>
+                                        </div>
+                                        <div className="col-lg-7 d-grid custom-small-text-12 align-items-center justify-content-end">
+                                            <p className="fw-light custom-small-text-12"> Current Products: <span className='fw-700'>50 / 100</span></p>
+                                        </div>
+                                    </div>
+
+                                    <ProgressBar now={40} label={4} className='mb-1' />
+
+                                    <p className="fw-light custom-small-text-12">
+                                        Total Amount:
+                                        <span className='fw-700'>
+                                            {machine.totalAmount}
+                                        </span>
+                                    </p>
+
+                                    <div class="d-grid gap-2 mt-4">
+                                        <button class="btn btn-danger rounded-full p-1" type="button">Refill</button>
+                                        <div className="row mt-2">
+                                            <div className="d-grid col-lg-6">
+                                                <button class="main-btn secondary-btn rounded-full p-1" type="button">All Products</button>
+                                            </div>
+                                            <div className="d-grid col-lg-6">
+                                                <button class="main-btn dark-btn rounded-full p-1" type="button">Transactions</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </React.Fragment>
+    )
+}
+
+export default Machine
